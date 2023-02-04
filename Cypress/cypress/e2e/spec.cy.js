eccd9617f308824640becb5a2e8706ee6c7db262
D0tNet4Fun <D0tNet4Fun@users.noreply.github.com>
2023-02-04 16:07:28 +0200

    Test debug, added configuration for VS Code (https://stackoverflow.com/questions/52502130/debugging-cypress-tests-in-visual-studio-code)

--- Cypress/cypress/e2e/spec.cy.js
+++ Cypress/cypress/e2e/spec.cy.js
@@ -3,7 +3,7 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io/todo')//.debug();
-    cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
+    cy.get('.todo-list li').debug().first().should('have.text', 'Pay electric bill')
     cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
     cy.get('.todo-list li').should('have.length', 2)
   })
